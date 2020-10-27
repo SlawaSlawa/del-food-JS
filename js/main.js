903c220bfdb82e991174c9cf5a890c98fa2bee17
SlawaSlawa <slawapocht4@yandex.ru>
2020-10-27 14:44:43 +0400

    Day 1 not home-work

--- js/main.js
+++ js/main.js
@@ -8,3 +8,78 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+// Элементы
+const buttonAuth = document.querySelector('.button-auth'),
+  modalAuth = document.querySelector('.modal-auth'),
+  closeAuth = document.querySelector('.close-auth'),
+  logInForm = document.getElementById('logInForm'),
+  loginInput = document.getElementById('login'),
+  userName = document.querySelector('.user-name'),
+  buttonOut = document.querySelector('.button-out');
+
+
+let login = localStorage.getItem('gloDelivery') || '';
+console.log('login: ', login);
+
+
+// Авторизация
+
+// Открытие закрытие модального окна авторизации
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+
+
+function autorized() {
+
+  function logOut() {
+    login = '';
+    localStorage.removeItem('gloDelivery');
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+
+  console.log('Autorized');
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+
+  buttonOut.addEventListener('click', logOut);
+}
+
+function notAutorized() {
+  console.log('notAutorized');
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+    localStorage.setItem('gloDelivery', login);
+    toggleModalAuth();
+    buttonAuth.removeEventListener('click', toggleModalAuth);
+    closeAuth.removeEventListener('click', toggleModalAuth);
+    logInForm.removeEventListener('submit', logIn);
+    logInForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit', logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    autorized();
+  } else {
+    notAutorized();
+  }
+}
+
+checkAuth();
